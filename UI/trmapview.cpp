145e069a91f4118512b514ab92c690c2d025031d
hubert <hubert-schmid@gmx.de>
2026-09-01 17:12:31 +0200

    Fix: wrong picture path (path vs. message)

--- UI/trmapview.cpp
+++ UI/trmapview.cpp
@@ -520,8 +520,8 @@ void TrMapView::on_nextTile(int x, int y)
 	if(m_tile->resetX(x,y))
 		return;
 
-	QString path = m_tile->getTilePath(x, y) + " (" + QString::number(m_tile->m_per) + "%)";
-	emit sendMessage(path, 0);
+	QString path = m_tile->getTilePath(x, y);
+	emit sendMessage(path +  + " (" + QString::number(m_tile->m_per) + "%)", 0);
 	m_tile->setLevelPath(x);
 	m_tile->createPngImageByPath(path);
 	--y;
